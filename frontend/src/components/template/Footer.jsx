c4bf9256aa2487828bbc1a69f7343b6148484d68
PomboObeso <matheus.gomes201@gmail.com>
2025-10-09 09:42:24 -0300

    Implementação do Footer+Icons

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,10 @@ import React from 'react'
 
 export default props => 
     <footer className="footer">
-        Rodape
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong> Cod<span className="text-danger">3</span>r</strong>
+        </span>
+        <span>
+            Implementado por Matheus G.O. <i className="fa fa-magic"></i><i className="fa fa-laptop"></i>
+        </span>
     </footer>
